a79669a6cedd04fb7f5118244a6717ca9c468974
Ed Preston <epreston@prestonsoft.com>
2023-08-27 22:03:04 +1000

    Update vite.config.js
    vendor chunk splitting for clarity

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 // vite.config.js
 
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    plugins: [splitVendorChunkPlugin()],
     // additional asset types
     assetsInclude: ['**/*.gltf', '**/*.glb', '**/*.m4a'],
     build: {
+        // minify: false,
+        // assetsInlineLimit: 0,
         target: [
             'es2022',
             'edge112',
@@ -14,6 +17,35 @@ export default defineConfig({
             'chrome112',
             'safari16.4',
             'ios16.4'
-        ]
+        ],
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    // creating a chunk to semver deps. Reducing the vendor chunk size
+                    if (
+                        id.includes('semver') ||
+                        id.includes('yallist') ||
+                        id.includes('lru-cache')
+                    ) {
+                        return 'semver';
+                    }
+                    // creating a chunk to playcanvas deps. Reducing the vendor chunk size
+                    if (
+                        id.includes('playcanvas') ||
+                        id.includes('@playcanvas')
+                    ) {
+                        return 'playcanvas';
+                    }
+                    // creating a chunk to spine-core deps. Reducing the vendor chunk size
+                    if (
+                        id.includes('spine41') ||
+                        id.includes('@esotericsoftware/spine-core') ||
+                        id.includes('spine-core')
+                    ) {
+                        return 'spine41';
+                    }
+                }
+            }
+        }
     }
 });
