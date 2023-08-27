e9e3d7125816fbbec3c1d506a5ef905dc66407b2
Ed Preston <epreston@prestonsoft.com>
2023-08-27 17:12:15 +1000

    Create spine.js
    editor file

---
+++ editor/spine.js
@@ -0,0 +1,32 @@
+/* this file is copied unmodified into the Editor */
+
+var Spine = pc.createScript("spine");
+
+Spine.attributes.add("atlas", { type: "asset", assetType: "text" });
+Spine.attributes.add("skeleton", { type: "asset", assetType: "json" });
+Spine.attributes.add("textures", { type: "asset", array: true, assetType: "texture" });
+Spine.attributes.add("priority", { type: "number", default: 1 });
+
+Spine.prototype.initialize = function () {
+    if (this.atlas && this.textures && this.skeleton) {
+        // If all assets are present, add the spine component to the entity
+        this.entity.addComponent("spine", {
+            atlasAsset: this.atlas.id,
+            textureAssets: this.textures.map(function (a) {
+                return a.id;
+            }),
+            skeletonAsset: this.skeleton.id
+        });
+
+        if (this.entity.spine) {
+            this.priority = this.priority ? this.priority : 0;
+            this.entity.spine.spine.priority = this.priority;
+        }
+    }
+
+    this.on('attr:priority', function (val) {
+        if (this.entity.spine) {
+            this.entity.spine.spine.priority = val;
+        }
+    }, this);
+};
